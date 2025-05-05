4d7e2b0b65f32b9b00ecb6c36879899bb3326947
Pragya Maheshwari <pragyam1403@gmail.com>
2025-05-06 03:41:52 +0530

    Initial Commit

---
+++ src/components/Grid.tsx
@@ -0,0 +1,93 @@
+import { twMerge } from "tailwind-merge";
+import { usePathfinding } from "../hooks/usePathfinding";
+import { MAX_COLS, MAX_ROWS } from "../utils/constants";
+import { Tile } from "./Tile";
+import { MutableRefObject, useState } from "react";
+import { checkIfStartOrEnd, createNewGrid } from "../utils/helpers";
+
+export function Grid( {
+    isVisulaizationRunningRef,
+}:{
+        isVisulaizationRunningRef: MutableRefObject<boolean>;
+}) {
+    const {grid, setGrid} = usePathfinding();
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
+    const handleMouseDown = (row: number, col:number) => {
+        if(isVisulaizationRunningRef.current || checkIfStartOrEnd(row, col)){
+            return;
+        }
+
+        setIsMouseDown(true);
+        const newGrid = createNewGrid(grid, row, col)
+        setGrid(newGrid);  
+    };
+
+    const handleMouseUp = (row: number, col: number) => {
+        if(isVisulaizationRunningRef.current || checkIfStartOrEnd(row, col)){
+            return;
+        }   
+        setIsMouseDown(false);
+    }
+
+    const handleMouseEnter = (row: number, col:number) => {
+        if(isVisulaizationRunningRef.current || checkIfStartOrEnd(row, col)){
+            return;
+        }
+
+        if(isMouseDown){
+            const newGrid = createNewGrid(grid, row, col);
+            setGrid(newGrid);
+        }
+    };
+
+
+    return (
+        <div
+          className={twMerge(
+            // Base Classes
+            "flex items-center flex-col justify-center border-sky-300 mt-10",
+
+            // Control Grid Height
+            `lg:min-h-[${MAX_ROWS * 17}px] 
+             md:min-h-[${MAX_ROWS * 15}px]
+             xs:min-h-[${MAX_ROWS * 8}px]
+             min-h-[${MAX_ROWS * 7}px]`,
+             
+             // controling Grid Width
+            `lg:w-[${MAX_COLS * 17}px] 
+            md:w-[${MAX_COLS * 15}px]
+            xs:w-[${MAX_COLS * 8}px]
+            w-[${MAX_COLS * 7}px]
+           `
+          )}
+        >
+
+            {grid.map((r, rowIndex) => (
+                <div key={rowIndex} className="flex">
+                    {r.map((tile,tileIndex)=> {
+                        const {row, col,isStart, isEnd, isPath, isTraversed, isWall} = tile;
+                        return(
+                            <Tile
+                            key={tileIndex}
+                            row={tile.row}
+                            col={tile.col}
+                            isEnd={isEnd}
+                            isStart={isStart}
+                            isPath={isPath}
+                            isTraversed={isTraversed}
+                            isWall={isWall}
+                            handleMouseDown = {() => handleMouseDown(row, col)}
+                            handleMouseUp = {()=> handleMouseUp(row, col)}
+                            handleMouseEnter = {()=> handleMouseEnter(row, col)}
+                            
+                            />
+                        )
+                    })}
+                </div>
+
+            ))}
+        </div>
+    );
+
+}
