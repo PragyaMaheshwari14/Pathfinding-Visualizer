8282b8bbecb12cf71301b7020605d4b91fbc8f5d
Pragya Maheshwari <pragyam1403@gmail.com>
2025-05-23 20:24:30 +0530

    feat(ui): updated UI components for better design and layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -73,12 +73,12 @@ export function Nav({
     }
     
     return (
-        <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-600 sm:px-5 px-0">
-            <div className="flex item-center lg:justify-between justify-center w-full sm:w-[52rem] ">
-               <h1 className="lg:flex hidden w-[40%] text-2xl pl-1">
+        <div className= "w-full px-3 sm:px-5 shadow-gray-600">
+            <div className="flex flex-wrap lg:flex-nowrap items-center justify-between gap-4 w-full max-w-[90rem] mx-auto py-4 px-2 sm:px-4">
+               <h1 className="hidden lg:block text-4xl pl-1 font-outfit whitespace-nowrap ">
                   Pathfinding Visualizer
                 </h1> 
-                <div className="flex sm:items-end items-center justify-start sm:justify-between sm:flex-row flex-col sm:space-y-0 space-y-3 sm:py-0 py-4 sm:space-x-4">
+                <div className="flex flex-col sm:flex-row justify-center items-center gap-3 sm:gap-4 w-full sm:w-auto">
                     <Select
                        label = "Maze"
                        value = {maze}
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -18,14 +18,18 @@ export function Select({
            <label className="text-xs text-gray-300 ml-1" htmlFor={label}>{label}</label>
            <select
                disabled={isDisabled}
-               className="bg-gray-700 disabled:pointer-events-none rounded-md cursor-pointer hover:bg-gray-800 transition ease-in active:ring-0 active:border-0 p-2 min-w-[200px] sm:min-w-full"
+               className="bg-[#262626] disabled:pointer-events-none rounded-md cursor-pointer hover:bg-[#1c1c1c] transition ease-in active:ring-0 active:border-0 p-2 min-w-[200px] sm:min-w-full"
                id={label}
                value={value}
                onChange={onChange}
            >
                {options.map((option) => (
-                  <option key={option.value} value={option.value}>
+                  <option 
+                    key={option.value} 
+                    value={option.value}
+                  >
                       {option.name}
+                      
                    </option>
                 ))}
            </select>
